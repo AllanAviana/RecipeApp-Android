59ed785bcb17b93ba446ab946f846e0616e537ab
Allan <allan.viana2003@gmail.com>
2024-11-27 02:35:06 -0300

    Added horizontal slide animations for screen transitions in the navigation graph.

--- src/main/java/com/example/recipeapp_android/presentation/navigation/AppNavGraph.kt
+++ src/main/java/com/example/recipeapp_android/presentation/navigation/AppNavGraph.kt
@@ -10,22 +10,49 @@ import com.example.recipeapp_android.presentation.screens.details.RecipeDetailsS
 import com.example.recipeapp_android.presentation.screens.home.HomeScreen
 import com.example.recipeapp_android.presentation.screens.welcome.WelcomeScreen
 import com.example.recipeapp_android.presentation.viewmodel.RecipeViewModel
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 
 @Composable
 fun AppNavGraph(navController: NavHostController = rememberNavController()){
     val recipeViewModel: RecipeViewModel = viewModel()
 
     NavHost(
+
         navController = navController,
-        startDestination = "welcome_screen"
-    ){
-        composable("welcome_screen"){
+        startDestination = "welcome_screen",
+        enterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { it },
+                animationSpec = tween(200)
+            )
+        },
+        exitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { -it },
+                animationSpec = tween(200)
+            )
+        },
+        popEnterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { -it },
+                animationSpec = tween(200)
+            )
+        },
+        popExitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { it },
+                animationSpec = tween(200)
+            )
+        }
+    ) {
+        composable("welcome_screen") {
             WelcomeScreen(recipeViewModel, navController)
         }
-        composable("home_screen"){
+        composable("home_screen") {
             HomeScreen(recipeViewModel, navController)
         }
-        composable("details_screen"){
+        composable("details_screen") {
             RecipeDetailsScreen(recipeViewModel)
         }
     }
